3c4973825c808b6a37b2c3f122b114cb37ea1986
kyreru <kyreru@yahoo.com>
2022-11-12 12:01:37 -0500

    version 3 nov12

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,11 +11,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText oTextInputEditTextPassword;
     Button oButtonLogin;
     FirebaseAuth oAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int REQUEST_CODE_GOOGLE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         oTextInputEditTextPassword=findViewById(R.id.textInputEditTextPassword);
         oButtonLogin=findViewById(R.id.btnlogin);
         oAuth=FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         oButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +66,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private void singInGoogle(){
+        Intent signIntent =mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, REQUEST_CODE_GOOGLE);
+    }
+    private void firebaseAuthWhitGoogle(String idToken){
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        oAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            String id=oAuth.getCurrentUser().getUid();
+                            checkUserExist(id);
+                        }else{
+                            Log.w("ERROR", "signInWithCredential:faulure", task.getException());
+                        }
+
+                    }
+                });
+    }
+
+    private void checkUserExist(String id) {
+
+    }
+
     private void login() {
         String email = oTextInputEditTextEmail.getText().toString();
         String password = oTextInputEditTextPassword.getText().toString();
